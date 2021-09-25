674d85d0b1932b529eeaa610f875363280f5d510
lucifer2355 <dgajjar999@gmail.com>
2021-09-25 22:33:26 +0530

    add goto in for loop

--- 14Loops/main.go
+++ 14Loops/main.go
@@ -14,4 +14,28 @@ func main() {
 	for i := range days {
 		fmt.Println(days[i])
 	}
+
+	for index, day := range days {
+		fmt.Printf("index is %v and value is %v\n", index, day)
+	}
+
+	rougeValue := 1
+
+	for rougeValue < 10 {
+		if rougeValue == 2 {
+			goto lco
+		}
+
+		if rougeValue == 5 {
+			// break
+			rougeValue++
+			continue
+		}
+
+		fmt.Println("Value is: ", rougeValue)
+		rougeValue++
+	}
+
+lco:
+	fmt.Println("Jumping to learncodeonline.in")
 }
