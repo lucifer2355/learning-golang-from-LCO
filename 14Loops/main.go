e46100279cd237b3087c3af722fb3f8a5ea7f79a
lucifer2355 <dgajjar999@gmail.com>
2021-09-25 22:24:24 +0530

    add genral for loop

--- 14Loops/main.go
+++ 14Loops/main.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to LOOPS in golang")
 
+	days := []string{"Sunday", "Tuesday", "Wednesday", "Friday", "Saturday"}
+
+	for d := 0; d < len(days); d++ {
+		fmt.Println(days[d])
+	}
 }
