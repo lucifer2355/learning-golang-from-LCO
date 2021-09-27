fb5453f17db6c41825cddeb368387b8d0357692f
lucifer2355 <dgajjar999@gmail.com>
2021-09-27 22:15:06 +0530

    add methods

---
+++ 16Methods/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//! No inheritance in golang; No super or parent
+func main() {
+	fmt.Println("Welcome to structs in golang")
+
+	dhruvil := User{"Dhruvil", "dhruvil@go.dev", true, 22}
+	fmt.Println(dhruvil)
+	fmt.Printf("Dhruvil details are %+v\n", dhruvil)
+	fmt.Printf("Name is %v and Email id %v\n", dhruvil.Name, dhruvil.Email)
+	dhruvil.GetStatus()
+	dhruvil.NewMail()
+	fmt.Printf("Dhruvil details are %+v\n", dhruvil)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (u User) GetStatus() {
+	fmt.Println("Is user active:", u.Status)
+}
+
+func (u User) NewMail() {
+	u.Email = "test@go.dev"
+	fmt.Println("Email of this user is:", u.Email)
+}
